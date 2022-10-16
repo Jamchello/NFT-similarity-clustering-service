3e46fe4c5a469c52de9cd50cfd4bfa01e085bf54
jamie michel <jamie.michel@hotmail.com>
2022-10-16 20:34:58 +0100

    added listings handler, and ingestion of active listings

--- ingestion/algoseas.go
+++ ingestion/algoseas.go
@@ -81,6 +81,46 @@ type AlgoSeasNote struct {
 	Standard  string `json:"standard"`
 }
 
+type AlgoSeasListingItem struct {
+	AssetInformation struct {
+		NName   string `json:"nName"`
+		Sk      string `json:"SK"`
+		Listing struct {
+			Date            string `json:"date"`
+			EscrowAddress   string `json:"escrowAddress"`
+			Expires         string `json:"expires"`
+			IsDutch         bool   `json:"isDutch"`
+			LogicSig        string `json:"logicSig"`
+			Marketplace     string `json:"marketplace"`
+			MinBidDelta     int    `json:"minBidDelta"`
+			NextPayout      string `json:"nextPayout"`
+			Seller          string `json:"seller"`
+			SnipeThreshold  int    `json:"snipeThreshold"`
+			Price           int    `json:"price"`
+			Quantity        int    `json:"quantity"`
+			Royalty         int    `json:"royalty"`
+			RoyaltyString   string `json:"royaltyString"`
+			VerifiedRoyalty bool   `json:"verifiedRoyalty"`
+			ListingID       int    `json:"listingID"`
+			VariableID      int    `json:"variableID"`
+		} `json:"listing"`
+	} `json:"assetInformation"`
+	MarketActivity struct {
+		AlgoAmount               int    `json:"algoAmount"`
+		AssetID                  int    `json:"assetID"`
+		CreationDate             string `json:"creationDate"`
+		CurrentAuctionAlgoAmount int    `json:"currentAuctionAlgoAmount"`
+		Event                    string `json:"event"`
+		ExpirationDate           string `json:"expirationDate"`
+		GroupTxnID               string `json:"groupTxnID"`
+		InitiatorAddress         string `json:"initiatorAddress"`
+		ListedAlgoAmount         int    `json:"listedAlgoAmount"`
+		MarketplaceID            string `json:"marketplaceID"`
+		PreviousOwner            string `json:"previousOwner"`
+		TxnID                    string `json:"txnID"`
+	} `json:"marketActivity"`
+}
+
 func ReadDataFromJson() []AlgoSeasHistoryItem {
 	// Open our jsonFile
 	jsonFile, err := os.Open("historicData.json")
@@ -96,6 +136,19 @@ func ReadDataFromJson() []AlgoSeasHistoryItem {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &data)
 	return data
+}
+
+func GetListings() []AlgoSeasListingItem {
+	listings := []AlgoSeasListingItem{}
+
+	res, err := http.Get("https://d3ohz23ah7.execute-api.us-west-2.amazonaws.com/prod/marketplace/listings?type=listing&sortBy=price&sortAscending=false&collectionName=AlgoSeas%20Pirates&limit=500")
+	if err != nil {
+		fmt.Println("Failed to fetch latest listings")
+	} else {
+		defer res.Body.Close()
+		json.NewDecoder(res.Body).Decode(&listings)
+	}
+	return listings
 }
 
 func GetSales() []AlgoSeasHistoryItem {
